c1200146a44b73a4e27fa1df44365424f5227a87
nickroberts404 <nickroberts404@gmail.com>
2016-04-18 01:28:17 -0500

    Added react and basic root component

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,4 +1,15 @@
 // app.js
-
+var React = require('react');
+var render = require('react-dom').render;
 var socket = require('socket.io-client')();
 console.log('Welcome to telegraph...');
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<p>Tap to begin a message</p>
+		)
+	}
+}
+
+render(<App />, document.getElementById('app'));
